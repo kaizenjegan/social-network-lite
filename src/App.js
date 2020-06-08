19dabfb82449e328f2554077f49391327912c7e5
Jegan Matthews <jegan.matthews@brownpapertickets.com>
2020-06-07 22:12:33 -0700

    added dashboard

--- src/App.js
+++ src/App.js
@@ -1,48 +1,118 @@
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
-import React, { useState, useEffect } from "react"
-import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles"
-import { ApolloProvider } from "react-apollo"
-import { ApolloClient } from "apollo-client"
-import { InMemoryCache } from "apollo-cache-inmemory"
-import { onError } from "apollo-link-error"
-import { ApolloLink } from "apollo-link"
-import { split } from "apollo-link"
-import { WebSocketLink } from "apollo-link-ws"
-import { getMainDefinition } from "apollo-utilities"
-import { createHttpLink } from "apollo-link-http"
-import { setContext } from "apollo-link-context"
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+import { ApolloProvider } from "react-apollo";
+import { ApolloClient } from "apollo-client";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { onError } from "apollo-link-error";
+import { ApolloLink } from "apollo-link";
+import { split } from "apollo-link";
+import { WebSocketLink } from "apollo-link-ws";
+import { getMainDefinition } from "apollo-utilities";
+import { createHttpLink } from "apollo-link-http";
+import { setContext } from "apollo-link-context";
 
-
-import { makeStyles, useTheme } from "@material-ui/core/styles"
+import { makeStyles, useTheme } from "@material-ui/core/styles";
 import {
-  HeaderImageContext,
-  headerImageReducer,
-  initialState as headerInitialState,
-  setHeaderImage,
-} from "./Contexts/HeaderImageContext"
+  SearchContext,
+  searchReducer,
+  initialState,
+  editSearch,
+} from "./Contexts/SearchContext";
 
-import logo from './logo.svg';
-import './App.css';
+import {
+  UserContext,
+  userReducer,
+  initialUserState,
+} from "./Contexts/UserContext";
+
+import { HomePage } from "./HomePage"
+
+
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  // Context for Search terms / genres / categories
+  const [searchInfo, dispatch] = React.useReducer(searchReducer, initialState);
+  const [userInfo, userDispatch] = React.useReducer(
+    userReducer,
+    initialUserState
   );
+
+  const createClient = () => {
+    const httpLink = createHttpLink({
+      uri: `https://localhost:3001/graphql`,
+    });
+
+    const authLink = setContext((_, { headers }) => {
+      const localToken = sessionStorage.getItem("auth");
+      console.log("localToken: ", localToken);
+      return {
+        headers: {
+          ...headers,
+          authorization: localToken
+            ? `Bearer ${JSON.parse(localToken).token}`
+            : "",
+        },
+      };
+    });
+
+    const wsLink = new WebSocketLink({
+      // TODO : fix so not hardcoded localhost
+      uri: `ws://localhost:3001/graphql`,
+      options: {
+        // reconnect: true,
+        connectionParams: {
+          authToken: localStorage.getItem("auth"),
+        },
+      },
+    });
+
+    const link = split(
+      ({ query }) => {
+        const { kind, operation } = getMainDefinition(query);
+        return kind === "OperationDefinition" && operation === "subscription";
+      },
+      wsLink,
+      authLink.concat(httpLink)
+    );
+
+    const client = new ApolloClient({
+      link,
+      cache: new InMemoryCache(),
+    });
+    return client;
+  };
+
+  const [client, setClient] = useState(createClient());
+
+  return (<div className="App">
+    <SearchContext.Provider value={dispatch}>
+          <UserContext.Provider value={userDispatch}>
+            <ApolloProvider client={client}>
+            {/* add ThemeProvider */}
+                <Router>
+                  <Switch>
+                    <Route
+                      exact
+                      path="/"
+                      render={(props) => {
+                        return (
+                          <HomePage
+                            {...props}
+                          />
+                        )
+                      }}
+                    />  
+                  </Switch>
+                
+              </Router>
+            </ApolloProvider>
+          </UserContext.Provider>
+    </SearchContext.Provider>
+    
+  </div>);
 }
 
 export default App;
---
+++ src/HomePage/HomePage.js
@@ -0,0 +1,11 @@
+import React from "react"
+import {DashboardCard } from "./DashboardCard"
+
+export const HomePage = (props) => {
+    return(
+        <div>
+            <div> Home </div>
+            <DashboardCard />
+        </div>
+    )
+}
