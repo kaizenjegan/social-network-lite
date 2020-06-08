54b6289ad155cc37c4b54cc3c8e6c0673eaa92a3
Jegan Matthews <jegan.matthews@brownpapertickets.com>
2020-06-07 18:02:22 -0700

    initial scaffold

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
+import React, { useState, useEffect } from "react"
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles"
+import { ApolloProvider } from "react-apollo"
+import { ApolloClient } from "apollo-client"
+import { InMemoryCache } from "apollo-cache-inmemory"
+import { onError } from "apollo-link-error"
+import { ApolloLink } from "apollo-link"
+import { split } from "apollo-link"
+mport { WebSocketLink } from "apollo-link-ws"
+import { getMainDefinition } from "apollo-utilities"
+import { createHttpLink } from "apollo-link-http"
+import { setContext } from "apollo-link-context"
+
+
+import { makeStyles, useTheme } from "@material-ui/core/styles"
+
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
