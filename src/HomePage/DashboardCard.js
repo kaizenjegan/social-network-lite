4b6db5bb89043fc7c4ed2c551fea42e88499e838
Jegan Matthews <jegan.matthews@brownpapertickets.com>
2020-06-08 20:26:02 -0700

    styled dashboard card

--- src/HomePage/DashboardCard.js
+++ src/HomePage/DashboardCard.js
@@ -16,15 +16,21 @@ import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
 import { makeStyles } from '@material-ui/core/styles';
 import clsx from 'clsx';
-
+import CardActionArea from '@material-ui/core/CardActionArea';
+import Button from '@material-ui/core/Button';
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    maxWidth: 345,
+    // paddingBottom: '50px'
+    marginBottom: "10px"
   },
   media: {
     height: 0,
     paddingTop: '56.25%', // 16:9
+    
+  },
+  fullCard: {
+    width: "100%"
   },
   expand: {
     transform: 'rotate(0deg)',
@@ -45,7 +51,7 @@ const useStyles = makeStyles((theme) => ({
 export const DashboardCard = (props) => {
   const classes = useStyles();
   const [expanded, setExpanded] = React.useState(false);
-  const [card, setCard] = React.useState([]);
+  const [cards, setCard] = React.useState([]);
 
 
   const handleExpandClick = () => {
@@ -55,81 +61,44 @@ export const DashboardCard = (props) => {
   useEffect(()=>{
     setCard(
       [{
-        body: "This ixmpressive paella is a perfect party dish and a fun meal to cook together with your guests. Add 1 cup of frozen peas along with the mussels, if you like."
+        body: "This ixmpressive paella is a perfect party dish and a fun meal to cook together with your guests. Add 1 cup of frozen peas along with the mussels, if you like.",
+        img: "/place-holder.jpeg"
       },{
-        body: "This impressive paella is a perfect party dish and a fun meal to cook together with your guests. Add 1 cup of frozen peas along with the mussels, if you like."
+        body: "This impressive paella is a perfect party dish and a fun meal to cook together with your guests. Add 1 cup of frozen peas along with the mussels, if you like.",
+        img: "/place-holder.jpeg"
       }]
     )
   },[])
 
     return (
-      card.map( card =>{
-        return  <Card className={classes.root}>
-            <CardHeader
-              avatar={
-                <Avatar aria-label="recipe" className={classes.avatar}>
-                  R
-                </Avatar>
-              }
-              action={
-                <IconButton aria-label="settings">
-                  <MoreVertIcon />
-                </IconButton>
-              }
-              title="Shrimp and Chorizo Paella"
-              subheader="September 14, 2016"
-            />
-            <CardContent>
-              <Typography variant="body2" color="textSecondary" component="p">
-                {card.body}
-              </Typography>
-            </CardContent>
-            <CardActions disableSpacing>
-              <IconButton aria-label="add to favorites">
-                <FavoriteIcon />
-              </IconButton>
-              <IconButton aria-label="share">
-                <ShareIcon />
-              </IconButton>
-              <IconButton
-                className={clsx(classes.expand, {
-                  [classes.expandOpen]: expanded,
-                })}
-                onClick={handleExpandClick}
-                aria-expanded={expanded}
-                aria-label="show more"
-              >
-                <ExpandMoreIcon />
-              </IconButton>
-            </CardActions>
-            <Collapse in={expanded} timeout="auto" unmountOnExit>
+      cards.map( card => 
+          {
+            return <Card className={classes.root}>
+            <CardActionArea>
               <CardContent>
-                <Typography paragraph>Method:</Typography>
-                <Typography paragraph>
-                  Heat 1/2 cup of the broth in a pot until simmering, add saffron and set aside for 10
-                  minutes.
+                <Typography gutterBottom variant="h5" component="h2">
+                  Lizard
                 </Typography>
-                <Typography paragraph>
-                  Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over medium-high
-                  heat. Add chicken, shrimp and chorizo, and cook, stirring occasionally until lightly
-                  browned, 6 to 8 minutes. Transfer shrimp to a large plate and set aside, leaving chicken
-                  and chorizo in the pan. Add pimentón, bay leaves, garlic, tomatoes, onion, salt and
-                  pepper, and cook, stirring often until thickened and fragrant, about 10 minutes. Add
-                  saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
-                </Typography>
-                <Typography paragraph>
-                  Add rice and stir very gently to distribute. Top with artichokes and peppers, and cook
-                  without stirring, until most of the liquid is absorbed, 15 to 18 minutes. Reduce heat to
-                  medium-low, add reserved shrimp and mussels, tucking them down into the rice, and cook
-                  again without stirring, until mussels have opened and rice is just tender, 5 to 7
-                  minutes more. (Discard any mussels that don’t open.)
-                </Typography>
-                <Typography>
-                  Set aside off of the heat to let rest for 10 minutes, and then serve.
+                <Typography variant="body2" color="textSecondary" component="p">
+                  {card.body}
                 </Typography>
               </CardContent>
-            </Collapse>
+              <CardMedia
+                className={classes.media}
+                image={card.img}
+                title="Contemplative Reptile"
+              />
+            </CardActionArea>
+            <CardActions>
+              <Button size="small" color="primary">
+                Share
+              </Button>
+              <Button size="small" color="primary">
+                Learn More
+              </Button>
+            </CardActions>
           </Card>
+          
         })
       );
 }
--- src/HomePage/HomePage.js
+++ src/HomePage/HomePage.js
@@ -1,9 +1,19 @@
 import React from "react"
 import {DashboardCard } from "./DashboardCard"
+import { makeStyles } from '@material-ui/core/styles';
 
+const useStyles = makeStyles((theme) => ({
+    root: {
+      // paddingBottom: '50px'
+      // marginBottom: "45px"
+      backgroundColor: '#cecece'
+    }
+  }));
+  
 export const HomePage = (props) => {
+    const classes = useStyles();
     return(
-        <div>
+        <div className={classes.root}>
             <DashboardCard />
         </div>
     )
