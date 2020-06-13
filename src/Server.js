0b66fd38973cf09747424e587aa654ac69b97bae
Jegan Matthews <jegan.matthews@brownpapertickets.com>
2020-06-13 10:53:32 -0700

    remove back slash

--- src/Server.js
+++ src/Server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-app.use(express.static(__dirname + '/'));
+// app.use(express.static(__dirname + '/'));
 app.listen(process.env.PORT || 8080);
 app.use('/', express.static(path.join(__dirname, 'public')))
 if (process.env.NODE_ENV === 'production') {
