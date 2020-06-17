d6c6b1ab5fae9512789d8dfe12f947e87dc089a2
Jegan Matthews <jegan.matthews@brownpapertickets.com>
2020-06-16 21:07:29 -0700

    fix css quirk

--- src/Component/SearchBar.js
+++ src/Component/SearchBar.js
@@ -44,7 +44,7 @@ const useStyles = makeStyles((theme) => ({
     inline: {
         margin: "10px",
         float: "left",
-        padding: '10px 121px 0px 12px',
+        padding: '10px 111px 0px 12px',
         fontSize: "16px"
     }
   }));
