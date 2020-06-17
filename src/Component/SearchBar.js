539bc9ce58629177f30d6f22410024853fe8dc06
Jegan Matthews <jegan.matthews@brownpapertickets.com>
2020-06-16 21:03:17 -0700

    remove zoom focus

--- src/Component/SearchBar.js
+++ src/Component/SearchBar.js
@@ -44,7 +44,8 @@ const useStyles = makeStyles((theme) => ({
     inline: {
         margin: "10px",
         float: "left",
-        padding: '10px 121px 0px 12px'
+        padding: '10px 121px 0px 12px',
+        fontSize: "16px"
     }
   }));
 
